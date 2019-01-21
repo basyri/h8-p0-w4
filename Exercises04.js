87f9caf04491deff4258bfba93acb87f7e9a7317
basyri <husain.basyri@gmail.com>
2019-01-21 10:29:40 +0700

    up

--- Exercises04.js
+++ Exercises04.js
@@ -4,8 +4,8 @@ Problem
 Diberikan sebuah function cariModus(arr) yang menerima sebuah array angka. 
 Function akan me-return modus dari array atau deret angka tersebut. 
 Modus adalah angka dari sebuah deret yang paling banyak atau paling sering muncul. 
-Contoh, modus dari [10, 4, 5, 2, 4] adalah 4. Jika modus tidak ditemukan, f
-unction akan me-return -1. Apabila ditemukan lebih dari dua nilai modus, 
+Contoh, modus dari [10, 4, 5, 2, 4] adalah 4. Jika modus tidak ditemukan, 
+function akan me-return -1. Apabila ditemukan lebih dari dua nilai modus, 
 tampilkan nilai modus yang paling pertama muncul (dihitung dari kiri ke kanan). 
 Dan apabila dialam modus hanya ada 1 nilai yang sama maka function akan me-return -1, 
 Contohnya [1, 1, 1] adalah -1.
@@ -13,22 +13,22 @@ Contohnya [1, 1, 1] adalah -1.
 
 function cariModus(arr) {
     // you can only write your code here!
-    var counter = [];
-    var mode = [];
-    var max = 0;
-    for (var i in arr) {
-        if (!(arr[i] in counter))
-            counter[arr[i]] = 0;
-        counter[arr[i]]++;
 
-        if (counter[arr[i]] == max)
-            mode.push(arr[i]);
-        else if (counter[arr[i]] > max) {
-            max = counter[arr[i]];
-            mode = [arr[i]];
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                return arr[i]
+            } else if (arr[i] === arr[arr.length - 2]) {
+                return -1
+            }
+            // if (arr[ji] % arr[j] === 0) {
+            //     return -1
+            // }
+
         }
     }
-    return mode;
+    // return temp
+
 }
 
 // TEST CASES
--- Exercises06-v3.js
+++ Exercises06-v3.js
@@ -35,8 +35,8 @@ function digitPerkalianMinimum(angka) {
     return digitlong
 }
 // // TEST CASES
-// console.log(digitPerkalianMinimum(24)); // 2
-// console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
 console.log(digitPerkalianMinimum(20)); // 2
-// console.log(digitPerkalianMinimum(179)); // 4
-// console.log(digitPerkalianMinimum(1)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
